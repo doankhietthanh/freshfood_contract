dd3ac114c3741c88da0156fc1f966427802068de
Phạm Tấn Minh Tiến <phamtanminhtien@gmail.com>
2023-03-26 18:44:56 +0700

    rewrite deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const FreshFood = await ethers.getContractFactory("FreshFood");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const freshFood = await FreshFood.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  await freshFood.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
