7752953f1939cb6eaaef226ad219f1f3b5739fc7
Phạm Tấn Minh Tiến <phamtanminhtien@gmail.com>
2023-05-16 22:07:51 +0700

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,15 @@ const SEPOLIA_PRIVATE_KEY =
   "844c3fcd6824f0141c87def50180af77cf7ab62289d17d16d404c8e22d9b3b79";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
   paths: {
     artifacts: "../freshfood-fe/src/artifacts",
   },
@@ -18,6 +26,10 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/ePW1u9KOntXwqMxIlLYstlx0ZiuGGPrg`,
       accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+    development: {
+      url: "http://localhost:8545",
+      blockGasLimit: 100000000,
     },
   },
 };
