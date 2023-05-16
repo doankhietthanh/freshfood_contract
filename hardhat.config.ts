d9dab7aa5120295e885a247a89610f7bf783c270
Phạm Tấn Minh Tiến <phamtanminhtien@gmail.com>
2023-05-16 20:11:18 +0700

    update network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const INFURA_API_KEY = "ePW1u9KOntXwqMxIlLYstlx0ZiuGGPrg";
+
+const SEPOLIA_PRIVATE_KEY =
+  "844c3fcd6824f0141c87def50180af77cf7ab62289d17d16d404c8e22d9b3b79";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   paths: {
@@ -8,6 +13,12 @@ const config: HardhatUserConfig = {
   },
   typechain: {
     outDir: "../freshfood-fe/src/types",
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/ePW1u9KOntXwqMxIlLYstlx0ZiuGGPrg`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
   },
 };
 
